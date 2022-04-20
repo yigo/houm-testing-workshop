0e9d3503c17b16b39c3bc128fab4af2075a19124
Rodrigo González <rodrigo.gonzalez@houm.com>
2022-04-20 16:58:59 -0400

    test2: challenge description

---
+++ src/Test2.tsx
@@ -0,0 +1,7 @@
+/* Challenge :
+** Fech a list and render a list of pokemons using a card.
+** at least 10 elements
+** the component should handle success state rending the elements
+** the component should handle loading state
+** the component should handle error state when request fails
+*/
