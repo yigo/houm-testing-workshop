71bdf6b3f666953c0568d28f18164353a283cc8c
Rodrigo González <rodrigo.gonzalez@houm.com>
2022-04-20 16:51:15 -0400

    calculator test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Should show sum of  two values", () => {
+  // prepare las variables del test o el escenario
+  const valueA = 2;
+  const valueB = 3;
+  const expected = 5;
+  // ejecutar el componente
+  const { debug } = render(<App />);
+  // encontrar los input en el codigo 
+  const inputA = screen.getByLabelText("valueA");
+  const inputB = screen.getByLabelText("valueB");
+
+
+  // simular escribir valores en los inputs
+  fireEvent.change(inputA, { target: { value: valueA }})
+  fireEvent.change(inputB, { target: { value: valueB }})
+
+  //console.log(debug());
+
+  // encontrar el buton calcular 
+  const button = screen.getByText("click mi");
+
+  // presioanr el boton 
+  fireEvent.click(button);  
+  //console.log(debug());
+  // hacer las aseveraciones
+  // buscar resultado esperado en el componente  
+  expect(screen.getByText(`Result: ${expected}`)).toBeInTheDocument()
+});
+
+test("Should not input value A when user types a NaN value", () => {
+  // prepare las variables del test o el escenario
+  const valueA = "a";
+  
+  // ejecutar el componente
+  const { debug } = render(<App />);
+  // encontrar los input en el codigo 
+  const inputA = screen.getByLabelText("valueA");
+
+  // simular escribir valores en los inputs
+  fireEvent.change(inputA, { target: { value: valueA }})
+
+  //console.log(debug());
+  // hacer las aseveracione∂s
+  // buscar resultado esperado en el componente  
+  expect(screen.queryByDisplayValue("a")).not.toBeInTheDocument();
+  //expect(inputA).toHaveAttribute("value", expectedValue);
+});
+
+test("Should not input value B when user types a NaN value", () => {
+  // prepare las variables del test o el escenario
+  const valueB = "b";
+  
+  // ejecutar el componente
+  const { debug } = render(<App />);
+  // encontrar los input en el codigo 
+  const inputB = screen.getByLabelText("valueB");
+
+  // simular escribir valores en los inputs
+  fireEvent.change(inputB, { target: { value: valueB }})
+
+  //console.log(debug());
+  // hacer las aseveracione∂s
+  // buscar resultado esperado en el componente  
+  expect(screen.queryByDisplayValue("b")).not.toBeInTheDocument();
+  //expect(inputA).toHaveAttribute("value", expectedValue);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-
+import React, {useState} from "react"
 function App() {
+  const [result, setResult] = useState(0)
+  const [valueA, setValueA] = useState(0)
+  const [valueB, setValueB] = useState(0)
+  
+  const handleChangeValue = (value: string, setter: (x: number) => void) => {
+    if (!isNaN( Number(value)) ) setter(Number(value))
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault(); 
+    setResult(valueA + valueB);
+  }
+
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" aria-label="valueA" value={valueA} onChange={(e) => handleChangeValue(e.target.value, setValueA)}/>
+        <input type="text" aria-label="valueB" value={valueB} onChange={(e) => handleChangeValue(e.target.value, setValueB)}/>
+        <button type="submit">click mi</button>
+      </form>
+      <span>Result: {result}</span>
+    </>
   );
 }
 
